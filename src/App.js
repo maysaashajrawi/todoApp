de8296a667911fdea1fafbdc5e6bdbfa21903dac
maysaashajrawi <maysaashajrawi@gmail.com>
2021-06-09 08:45:46 +0300

    webpack

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import TodoForm from "./components/TodoForm";
+// import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList"
 function App() {
   return (
     <div className="App">
-            <TodoForm/>
+
             <TodoList/>
 
     </div>
